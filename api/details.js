d4c5bef255ad00b2808574ae699af7c152d783de
Ajanth Uthayan <uthayakumaran.ajanth@gmail.com>
2022-05-04 10:19:47 -0400

    set fetch method to GET

--- api/details.js
+++ api/details.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.get("/", (req, res) => {
   const link = req.body.magicEdenLink;
   const splitLink = link.split("/");
   const mintAddress = splitLink[splitLink.length - 1];
